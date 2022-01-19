3ab2bd5f360fd45a0cb39e3fab345cb3c1524677
ParanoidAndroid5 <mislinacetineer@gmail.com>
2022-01-19 21:26:54 +0300

    itemAdd function added

---
+++ ödev10/app.js
@@ -0,0 +1,30 @@
+//elementleri secmek icin
+let taskDOM = document.querySelector("#task")
+let btnDOM  = document.querySelector("#liveToastBtn") 
+let ulDOM   = document.querySelector("#list") 
+let ullength = document.getElementsByTagName("li")
+
+//buton icin eventListener 
+//btnDOM.addEventListener("click",itemAdd)
+
+// ekle yapan fonksiyon
+function itemAdd(){
+    let liDOM = document.createElement('li') // yeni li yarat
+    ulDOM.appendChild(liDOM) // bunları ulnin sonuna ekle
+    liDOM.innerHTML = taskDOM.value 
+    
+    
+
+    
+}
+// silme işlemini yapan fonksiyon
+function itemDelete(){
+
+}
+// yapılan işi işaretleyen fonksiyon 
+function itemMarked(){
+}
+//bildirimleri yansıtan fonksiyon
+function toast(){
+
+}
